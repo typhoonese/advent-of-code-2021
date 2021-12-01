aabb7acff69981c9c997b248a904f0d07adcd3da
typhoonese <typhoonese@outlook.com>
2021-12-01 22:20:02 +0000

    Create rust file and move rust code

---
+++ rust/day1/target/debug/day1.d
@@ -0,0 +1,1 @@
+/home/nufyatese/code/adventOfCode/rust/day1/target/debug/day1: /home/nufyatese/code/adventOfCode/rust/day1/src/main.rs
---
+++ rust/day1/target/debug/deps/day1-154db5457e203e1a.d
@@ -0,0 +1,5 @@
+/home/nufyatese/code/adventOfCode/rust/day1/target/debug/deps/day1-154db5457e203e1a: src/main.rs
+
+/home/nufyatese/code/adventOfCode/rust/day1/target/debug/deps/day1-154db5457e203e1a.d: src/main.rs
+
+src/main.rs:
