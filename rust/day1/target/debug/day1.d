2e60b1d7d4709cb6596864750557c542ef2693fc
typhoonese <typhoonese@outlook.com>
2021-12-01 22:25:10 +0000

    Revert "Create rust file and move rust code"
    This reverts commit aabb7acff69981c9c997b248a904f0d07adcd3da.

--- rust/day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/nufyatese/code/adventOfCode/rust/day1/target/debug/day1: /home/nufyatese/code/adventOfCode/rust/day1/src/main.rs
--- rust/day1/target/debug/deps/day1-154db5457e203e1a.d
+++
@@ -1,5 +0,0 @@
-/home/nufyatese/code/adventOfCode/rust/day1/target/debug/deps/day1-154db5457e203e1a: src/main.rs
-
-/home/nufyatese/code/adventOfCode/rust/day1/target/debug/deps/day1-154db5457e203e1a.d: src/main.rs
-
-src/main.rs:
